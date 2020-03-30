fcafb8b31ba4edc4a2199e71fd04ecdad8b5e840
Fabricio Mendes <fabriciofbmendes5@gmail.com>
2020-03-29 22:02:16 -0300

    Front-end do app

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,24 +2,24 @@ const connection = require('../database/connection');
 
 module.exports = {
     async index(request, response) {
-        const { page = 1} = request.query;
+     const { page = 1} = request.query;
 
-        const [count] = await connection('incidents').count();
+     const [count] = await connection('incidents').count();
         
          
         
-        const incidents = await connection('incidents')
-         .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-         .limit(5)
-         .offset((page - 1) * 5)
-         .select(['incidents.*',
-         'ongs.name',
-         'ongs.email',
-         'ongs.whatsapp',
-         'ongs.city',
-         'ongs.uf' 
+     const incidents = await connection('incidents')
+          .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+          .limit(5)
+          .offset((page - 1) * 10)
+          .select(['incidents.*',
+           'ongs.name',
+           'ongs.email',
+           'ongs.whatsapp',
+           'ongs.city',
+           'ongs.uf' 
         ]);
-        response.header('X-Total-Count', count['count(*)']);
+     response.header('X-Total-Count', count['count(*)']);
         
         return response.json(incidents);
     },
---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import './styles.css';
+import api from '../../services/api';
+
+export default function Profile() {
+    const [incidents, setIncidents] = useState([]);
+
+    const ongName = localStorage.getItem('ongName');
+    const ongId = localStorage.getItem('ongId');
+
+    useEffect(() => {
+      api.get('profile', {
+          headers: {
+              Authorization: ongId,
+          }
+      }).then(response => {
+        setIncidents(response.data);
+      })
+    }, [ongId]);
+
+    return(
+        <div className="profile-container">
+            <header>
+               <img src={logoImg} alt="Be The Hero" />
+               <span> Bem vinda, {ongName} </span>
+
+               <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+               <button type="button">
+                   <FiPower size={18} color="#E02041" />
+               </button>
+            </header>
+
+            <h1>Casos Cadastrados</h1>
+
+            <ul>
+                {incidents.map(incident => (
+                    <li key={incident.id}>
+                    <strong>CASO:</strong>
+                    <p>{incident.title}</p>
+
+                    <strong>DESCRIÇÂO:</strong>
+                    <p>{incident.description}</p>
+
+                    <strong>VALOR:</strong>
+                    <p>{incident.value}</p>
+
+                    <button type="button">
+                      <FiTrash2 size={20} color="#a8a8b3"/>
+                    </button>
+                </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
